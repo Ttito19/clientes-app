26e3bcc6cabcafb39ede7288dac1c8115fecf539
Ttito19 <andrezchavez.2000@gmaail.com>
2020-04-10 07:10:27 -0500

    manejo de errores y validación de formulario

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Injectable({
 	providedIn: 'root'
 })
 export class ClienteService {
 	private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 	private httpHeaders = new HttpHeaders({ 'Content-type': 'application/json' });
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient, private router: Router) {}
 	//metodo getClientes que retorna los CLIENTES
 	getClientes(): Observable<Cliente[]> {
 		//pipe permite agregar operador como el map
@@ -18,24 +20,56 @@ export class ClienteService {
 	}
 	//método para crear cliente
 	create(cliente: Cliente): Observable<Cliente> {
-		return this.http.post<Cliente>(this.urlEndPoint, cliente, {
-			headers: this.httpHeaders
-		});
+		return this.http
+			.post(this.urlEndPoint, cliente, {
+				headers: this.httpHeaders
+			})
+			.pipe(
+				map((reponse: any) => reponse.cliente as Cliente),
+				catchError((e) => {
+					console.error(e.error.mensaje);
+					swal.fire(e.error.mensaje, e.error.error, 'error');
+					return throwError(e);
+				})
+			);
 	}
 	//método para obtener la informacion del cliente por id
 	getCliente(id): Observable<Cliente> {
-		return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+		return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+			catchError((e) => {
+				this.router.navigate([ '/clientes' ]);
+				swal.fire('Error al editar', e.error.mensaje, 'error');
+				console.error(e.error.mensaje);
+				return throwError(e);
+			})
+		);
 	}
 	//método para actualizar
-	update(cliente: Cliente): Observable<Cliente> {
-		return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {
-			headers: this.httpHeaders
-		});
+	update(cliente: Cliente): Observable<any> {
+		return this.http
+			.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {
+				headers: this.httpHeaders
+			})
+			.pipe(
+				catchError((e) => {
+					console.error(e.error.mensaje);
+					swal.fire(e.error.mensaje, e.error.error, 'error');
+					return throwError(e);
+				})
+			);
 	}
 	//método para eliminar
 	delete(id: number): Observable<Cliente> {
-		return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {
-			headers: this.httpHeaders
-		});
+		return this.http
+			.delete<Cliente>(`${this.urlEndPoint}/${id}`, {
+				headers: this.httpHeaders
+			})
+			.pipe(
+				catchError((e) => {
+					console.error(e.error.mensaje);
+					swal.fire(e.error.mensaje, e.error.error, 'error');
+					return throwError(e);
+				})
+			);
 	}
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,63 +1,53 @@
-import { Component, OnInit } from "@angular/core";
-import { Cliente } from "./cliente";
-import { ClienteService } from "./cliente.service";
-import { Router, ActivatedRoute } from "@angular/router";
-import swal from "sweetalert2";
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 @Component({
-  selector: "app-form",
-  templateUrl: "./form.component.html",
-  // styleUrls: [ './form.component.css' ]
+	selector: 'app-form',
+	templateUrl: './form.component.html'
+	// styleUrls: [ './form.component.css' ]
 })
 export class FormComponent implements OnInit {
-  private cliente: Cliente = new Cliente();
-  private titulo: string = "Crear cliente";
-  constructor(
-    private clienteService: ClienteService,
-    private router: Router,
-    private ActivatedRoute: ActivatedRoute
-  ) {}
+	private cliente: Cliente = new Cliente();
+	private titulo: string = 'Crear cliente';
+	constructor(
+		private clienteService: ClienteService,
+		private router: Router,
+		private ActivatedRoute: ActivatedRoute
+	) {}
 
-  ngOnInit() {
-    this.cargarCliente();
-  }
-//listar por id 
-  cargarCliente(): void {
-    this.ActivatedRoute.params.subscribe((params) => {
-      let id = params["id"];
-      if (id) {
-        this.clienteService
-          .getCliente(id)
-          .subscribe((cliente) => (this.cliente = cliente));
-      }
-    });
-  }
-  //crear cliente
-  public create(): void {
-    //me trae el valor del input
-    console.log(this.cliente);
-    //llamo a create de cliente service
-    this.clienteService.create(this.cliente).subscribe(
-      ///redireccion a clientes
-      (cliente) => {
-		this.router.navigate(["/clientes"]);
-		// libreria de alerta
-        swal.fire(
-          "Nuevo cliente",
-          `Cliente ${cliente.nombre} creado con éxito`,
-          "success"
-        );
-      }
-    );
-  }
-  update():void{
-    this.clienteService.update(this.cliente).subscribe(cliente=>{
-      this.router.navigate(['/clientes'])
-     // libreria de alerta
-     swal.fire(
-      "Nuevo actualizado",
-      `Cliente ${cliente.nombre} actualizado con éxito`,
-      "success"
-    );
-    })
-  }
+	ngOnInit() {
+		this.cargarCliente();
+	}
+	//listar por id
+	cargarCliente(): void {
+		this.ActivatedRoute.params.subscribe((params) => {
+			let id = params['id'];
+			if (id) {
+				this.clienteService.getCliente(id).subscribe((cliente) => (this.cliente = cliente));
+			}
+		});
+	}
+	//crear cliente
+	public create(): void {
+		//me trae el valor del input
+		console.log(this.cliente);
+		//llamo a create de cliente service
+		this.clienteService.create(this.cliente).subscribe(
+			///redireccion a clientes
+			(cliente) => {
+				this.router.navigate([ '/clientes' ]);
+				// libreria de alerta
+				swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito`, 'success');
+			}
+		);
+	}
+	update(): void {
+		this.clienteService.update(this.cliente).subscribe((json) => {
+			this.router.navigate([ '/clientes' ]);
+			// libreria de alerta
+			swal.fire('Nuevo actualizado', `Cliente ${json.cliente.nombre} actualizado con éxito`, 'success');
+		});
+	}
 }
